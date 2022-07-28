f0778dff6eb9049c09b110e5a86ba4c993103456
other <other@mail.ru>
2022-07-28 20:53:04 +0300

    Screen keyboard

--- java-example/src/test/java/MyFirstTest.java
+++ java-example/src/test/java/MyFirstTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 /**
@@ -21,12 +23,19 @@ public class MyFirstTest {
     @Before
     public void start(){
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest(){
         driver.get("http://www.google.com");
+        //open screen keyboard
+        driver.findElement(By.cssSelector("span.ly0Ckb")).click();
+        driver.findElement(By.id("K32")).click(); //add space
+        //close screen keyboard
+        driver.findElement(By.cssSelector("span.ly0Ckb")).click();
+        //driver.findElement(By.xpath("//div[@class='vk-t-btn vk-sf-cl']")).click();
         driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
         wait.until(titleIs("webdriver - Поиск в Google"));
     }
