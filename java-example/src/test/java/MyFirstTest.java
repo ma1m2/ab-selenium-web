6ceb6e2f0ef4c3d07b6c835c47192822609ef558
other <other@mail.ru>
2022-08-13 09:57:04 +0300

    3.3_Capabilities

--- java-example/src/test/java/MyFirstTest.java
+++ java-example/src/test/java/MyFirstTest.java
@@ -1,10 +1,9 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -22,13 +21,29 @@ public class MyFirstTest {
 
     @Before
     public void start(){
-        driver = new ChromeDriver();
+        Capabilities caps = new ImmutableCapabilities("unhandledPromptBehavior", "dismiss"); //unhandledPromptBehavior//unexpectedAlertBehaviour
+        //driver = new ChromeDriver(caps);
+        //driver = new FirefoxDriver();
+        driver = new InternetExplorerDriver(caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest(){
+        driver.get("http://www.google.com");
+        wait.until(titleIs("Google"));
+//        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
+//        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    /**
+     * don't work for IE
+     * Unable to find element with css selector == span.ly0Ckb
+     */
+    @Test
+    public void screenKeyboardTest(){
         driver.get("http://www.google.com");
         //open screen keyboard
         driver.findElement(By.cssSelector("span.ly0Ckb")).click();
